ba19cbfdbc73eefe96e0b6dcadbb32c8ea2c58ec
sky-ash <sky-ash-dev@proton.me>
2025-02-13 16:28:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+(async function checkForUpdates() {
+    const currentVersion = "1.0";
+    const versionUrl = "https://raw.githubusercontent.com/ivysone/Will-you-be-my-Valentine-/main/version.json"; 
+
+    try {
+        const response = await fetch(versionUrl);
+        if (!response.ok) {
+            console.warn("Could not fetch version information.");
+            return;
+        }
+        const data = await response.json();
+        const latestVersion = data.version;
+        const updateMessage = data.updateMessage;
+
+        if (currentVersion !== latestVersion) {
+            alert(updateMessage);
+        } else {
+            console.log("You are using the latest version.");
+        }
+    } catch (error) {
+        console.error("Error checking for updates:", error);
+    }
+})();
+
+const messages = [
+    "u sure?",
+    "really sure??",
+    "please?",
+    "pretty please??",
+    "<- click here",
+    "<- THIS BUTTON",
+    "wiwiwi >:(",
+    "WI WI WI >:(",
+    "i will stop asking if you say yes!",
+    "fine, i will stop asking...",
+    ":("
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.1}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes.html";
+}
